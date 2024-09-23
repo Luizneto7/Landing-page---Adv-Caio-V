755eb6ebd82cd29a57aaeeb7f1b988f18569a086
caioadv <caiovianaandrade@outlook.com>
2024-09-23 17:54:53 -0300

    index.html e style.css update

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
       }, 2000);
   });
 });
+
+document.getElementById('hamburger').addEventListener('click', function() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.toggle('show');
+});
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      const targetTitle = targetSection.querySelector('.section-title');
+
+      window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: 'smooth'
+      });
+
+      targetTitle.classList.add('highlight');
+
+      setTimeout(() => {
+          targetTitle.classList.remove('highlight');
+      }, 2000);
+  });
+});
